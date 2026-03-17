7923a6622bff46b436b9e9deb3bdb63ff2213ffe
yuuhei-koutoku <yhkt.0627@gmail.com>
2026-03-17 10:59:36 +0900

    Add user following

--- src/test/integration/users_profile_test.rb
+++ src/test/integration/users_profile_test.rb
@@ -13,6 +13,8 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
     assert_select 'title', full_title(@user.name)
     assert_select 'h1', text: @user.name
     assert_select 'h1>img.gravatar'
+    assert_select 'a[href=?]', following_user_path(@user), text: "#{@user.following.count} following"
+    assert_select 'a[href=?]', followers_user_path(@user), text: "#{@user.followers.count} followers"
     assert_match @user.microposts.count.to_s, response.body
     assert_select 'div.pagination', count: 1
     @user.microposts.paginate(page: 1).each do |micropost|
